77540901bc9c6f56d05950e00ebca65bb509cabc
Jakub <jkostka-zawadzki@st.swps.edu.pl>
2026-01-14 11:17:44 +0100

    Okej

--- TeaSoul/TeaSoul/posts/admin.py
+++ TeaSoul/TeaSoul/posts/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+# modele musimy zaimportować
 from .models import Category, Topic
 
+# a następnie zarejestrować (pokazano najprostszy przypadek)
 admin.site.register(Category)
 admin.site.register(Topic)
---
+++ TeaSoul/models.py
@@ -0,0 +1,8 @@
+class Category(models.Model):
+    name = models.CharField(max_length=60)
+
+
+class Topic(models.Model):
+    name = models.CharField(max_length=60)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True) 
